5849b4c7bff9ae1ebc71c7ac5d433d567541836a
Soares007 <soaresstar947@gmail.com>
2023-04-11 15:37:18 -0300

    Adicionando os outros valores para conversão

--- script.js
+++ script.js
@@ -2,26 +2,24 @@ const valorReal = document.getElementById("valor_real");
 const cambioDolar = document.getElementById("cambio_dolar");
 const cambioEuro = document.getElementById("cambio_euro");
 const cambioPalm = document.getElementById("cambio_palm");
-const buttonConvert = document.getElementById("convert_btn");
+const buttonConvert = document.getElementById("converter_btn");
 const valorDolar = document.getElementById("valor_dolar");
 const valorEuro = document.getElementById("valor_euro");
 const valorPalm = document.getElementById("valor_palm");
 
 function converter() {
     const real = parseFloat(valorReal.value);
-    const cotacaoDolar = parseFloat(cambioDolar.value);
+    const cotacao_dolar = parseFloat(cambioDolar.value);
+    const cotacao_euro = parseFloat(cambioEuro.value);
 
     //Conversão Dólar
-    const dolar = real / cotacaoDolar;
+    const dolar = real / cotacao_dolar;
+    const euro = real / cotacao_euro;
 
     //Exibir o Valor do dólar
-    
-    //Conversão Euro
-    euro = real / cotacaoEuro;
+    valorDolar.value = `${dolar}`;
 
-    //Conversão Palms
-    palm = real / cotacaoPalm;
-
+    valorEuro.value = `${euro}`;
 }
 
 buttonConvert.addEventListener("click", converter);
