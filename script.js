2b39e72cfbd1112764edb8e72df82314002bef35
Soares007 <soaresstar947@gmail.com>
2023-04-11 14:10:15 -0300

    Criação da Estrutura da página

---
+++ script.js
@@ -0,0 +1,13 @@
+const valorReal = document.getElementById("valorReal");
+const cambioDolar = document.getElementById("cambioDolar");
+const cambioEuro = document.getElementById("cambioEuro");
+const cambioPalm = document.getElementById("cambioPalm");
+const buttonConvert = document.getElementById("convertBtn");
+const valorDolar = document.getElementById("valorDolar");
+const valorEuro = document.getElementById("valorEuro");
+const valorPalm = document.getElementById("valorPalm");
+
+function converter ()
+{
+    
+}
